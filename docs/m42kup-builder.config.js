e367ce0ccc8a66a34eaaef49b861b062960ba9bf
logico-philosophical <osialfecanakma@gmail.com>
2020-05-30 13:44:31 +0900

    Add some docs

--- docs/m42kup-builder.config.js
+++ docs/m42kup-builder.config.js
@@ -1,18 +1,22 @@
 var m42kup = require('m42kup'),
-  hljs = require('highlight.js'),
-  katex = require('katex');
+	hljs = require('highlight.js'),
+	katex = require('katex');
 
 m42kup.set({hljs, katex});
 
 module.exports = {
-  name: 'M42/math 명세',
-  src: 'src',
-  dst: 'build',
-  render: text => m42kup.render(text),
-  list: [
-    {
-      name: 'ExpressionResolver의 동작방식',
-      file: 'expression-resolver'
-    }
-  ]
+	name: 'M42/math 명세',
+	src: 'src',
+	dst: 'build',
+	render: text => m42kup.render(text),
+	list: [
+		{
+			name: '모형',
+			file: 'model'
+		},
+		{
+			name: 'ExpressionResolver의 동작방식',
+			file: 'expression-resolver'
+		}
+	]
 };
