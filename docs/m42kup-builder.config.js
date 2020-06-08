35e12172cb3e48d25b10c7287ec2e9bd6a3ec3e5
logico-philosophical <osialfecanakma@gmail.com>
2020-06-08 22:22:37 +0900

    Elaborate

--- docs/m42kup-builder.config.js
+++ docs/m42kup-builder.config.js
@@ -11,16 +11,22 @@ module.exports = {
 	render: text => m42kup.render(text),
 	list: [
 		{
-			name: '모형',
-			file: 'model'
-		},
-		{
-			name: 'D 함수와 증명연쇄에 관하여',
-			file: 'd'
-		},
-		{
-			name: 'U 함수에 관하여',
-			file: 'u'
+			name: '수학적 모형',
+			dir: 'model',
+			list: [
+				{
+					name: '대강의 모형',
+					file: 'model'
+				},
+				{
+					name: 'D 함수와 증명연쇄에 관하여',
+					file: 'd'
+				},
+				{
+					name: 'U 함수에 관하여',
+					file: 'u'
+				}
+			]
 		},
 		{
 			name: 'ExpressionResolver의 동작방식',
