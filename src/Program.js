5335871f94b7fd2e2a04d8cb333f01e80746039f
logico-philosophical <50512456+logico-philosophical@users.noreply.github.com>
2020-09-13 03:47:54 +0900

    Que sera sera

--- src/Program.js
+++ src/Program.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var Scope = require('./Scope');
 
 var Type = require('./nodes/Type');
@@ -15,39 +17,42 @@ var StackTrace = require('./StackTrace');
 
 ExpressionResolver.init({Type, Typevar, Fun, Funcall, Tee, Ruleset, Schema, Schemacall});
 
-function Program(filename) {
-	this.scope = new Scope(null);
-	this.trace = new StackTrace([], filename);
-	this.ExpressionResolver = ExpressionResolver;
+class Program {
+	constructor(filename) {
+		this.scope = new Scope(null);
+		this.trace = new StackTrace([], filename);
+	}
+
+	feed(lines, nativeMap) {
+		lines.forEach(line => {
+			switch (line._type) {
+				case 'typedef':
+					var type = PegInterface.type(line, this.scope, this.trace);
+					this.scope.addType(type);
+					break;
+				case 'defv':
+					var typevar = PegInterface.typevar(line, this.scope, this.trace);
+					this.scope.addTypevar(typevar);
+					break;
+				case 'defun':
+					var fun = PegInterface.fun(line, this.scope, this.trace);
+					this.scope.addFun(fun);
+					break;
+				case 'defruleset':
+					var ruleset = PegInterface.ruleset(line, this.scope, this.trace, nativeMap);
+					this.scope.addRuleset(ruleset);
+					break;
+				case 'defschema':
+					var schema = PegInterface.schema(line, this.scope, this.trace, nativeMap);
+					this.scope.addSchema(schema);
+					break;
+				default:
+					throw Error(`Unknown line type ${line._type}`);
+			}
+		});
+	}
 }
 
-Program.prototype.feed = function (lines, nativeMap) {
-	lines.forEach(line => {
-		switch (line._type) {
-			case 'typedef':
-				var type = PegInterface.type(line, this.scope, this.trace);
-				this.scope.addType(type);
-				break;
-			case 'defv':
-				var typevar = PegInterface.typevar(line, this.scope, this.trace);
-				this.scope.addTypevar(typevar);
-				break;
-			case 'defun':
-				var fun = PegInterface.fun(line, this.scope, this.trace);
-				this.scope.addFun(fun);
-				break;
-			case 'defruleset':
-				var ruleset = PegInterface.ruleset(line, this.scope, this.trace, nativeMap);
-				this.scope.addRuleset(ruleset);
-				break;
-			case 'defschema':
-				var schema = PegInterface.schema(line, this.scope, this.trace, nativeMap);
-				this.scope.addSchema(schema);
-				break;
-			default:
-				throw Error(`Unknown line type ${line._type}`);
-		}
-	});
-};
+Program.prototype.ExpressionResolver = ExpressionResolver;
 
 module.exports = Program;
