160846d212a6d7fec8eebe0ed9b061d47e9fe390
logico-philosophical <50512456+logico-philosophical@users.noreply.github.com>
2020-11-04 23:11:32 +0900

    Update test.ts

--- test/test.ts
+++ test/test.ts
@@ -9,13 +9,10 @@ var path = require('path');
 var grammar = fs.readFileSync(path.join(__dirname, '../src/grammar.pegjs'), 'utf-8');
 var parser = pegjs.generate(grammar, {cache: true});
 var evalParser = pegjs.generate(grammar, {cache: true, allowedStartRules: ['evaluable']});
-var loader = (filename: string) => {
-	return fs.readFileSync(filename + '.math', 'utf-8');
-};
-
-var program = new Program(parser);
 
 describe('Program', function () {
+	var program = new Program(parser);
+
 	[
 		'propositional', 'predicate', 'set',
 		'relation', 'function', 'natural',
@@ -37,6 +34,8 @@ describe('ObjectFun', function () {
 
 describe('Issue #52', function () {
 	it('(f(x))(y) == (f(x))(y)', async function () {
+		var program = new Program(parser);
+
 		await program.loadModule('duh', (_filename: string) => `
 type cls;
 
@@ -48,5 +47,27 @@ cls y;
 			baz = program.evaluate(evalParser.parse(`(f(x))(y)`)) as Metaexpr;
 		
 		expect(foo.equals(baz)).to.be.true;
+	});
+});
+
+describe('Sealed macro', function () {
+	it('N(p) != (sealed p => N(p))', async function () {
+		var program = new Program(parser);
+		
+		await program.loadModule('duh', (_filename: string) => `
+type st;
+
+st p;
+
+st N(st p);
+
+sealed st N2(st p) {
+	N(p)
+}
+`);
+		var foo = program.evaluate(evalParser.parse(`N(p)`)) as Metaexpr,
+			baz = program.evaluate(evalParser.parse(`N2(p)`)) as Metaexpr;
+		
+		expect(foo.equals(baz)).to.be.false;
 	});
 });
