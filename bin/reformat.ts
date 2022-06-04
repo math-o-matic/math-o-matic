a9334be0267e767672ae1422db4d365b4d573c29
logico-philosophical <ossia@korea.ac.kr>
2022-06-05 01:19:00 +0900

    Fix what's been broken

--- bin/reformat.ts
+++ bin/reformat.ts
@@ -1,24 +1,29 @@
-#!/usr/bin/env node
-
 import unparse from "../src/Unparser";
 import fs from 'fs';
 import path from 'path';
 import pegjs from 'pegjs';
+import { glob } from "glob";
+import mkdirp from 'mkdirp';
 
 var grammar = fs.readFileSync(path.join(__dirname, '../src/grammar.pegjs'), 'utf-8');
 var parser = pegjs.generate(grammar, {cache: true});
 
-var src = path.join(__dirname, '../math');
-var dst = path.join(__dirname, '../math-reformatted');
+var src = path.join(__dirname, '../math/');
+var dst = path.join(__dirname, '../math-reformatted/');
 
 if (!fs.existsSync(dst)) {
 	fs.mkdirSync(dst);
 }
 
-fs.readdirSync(src).forEach(file => {
-	var srcfile = path.join(src, file);
-	var code = fs.readFileSync(srcfile, 'utf-8');
+glob.sync('**/*.math', {cwd: src}).forEach(file => {
+	var srcFile = path.join(src, file);
+	var code = fs.readFileSync(srcFile, 'utf-8');
 
 	var newcode = unparse(parser.parse(code));
+	
+	var dstFile = path.join(dst, file);
+	var dstDir = path.dirname(dstFile);
+
+	mkdirp.sync(dstDir);
 	fs.writeFileSync(path.join(dst, file), newcode);
 });
